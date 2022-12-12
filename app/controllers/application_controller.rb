2e331f1a3a7a35efa57e0e94564c305825b06645
Beulah Mathenge <beulahmathenge@gmail.com>
2022-12-12 09:39:52 +0300

    App controller setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
     include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+
+    skip_before_action :verify_authenticity_token
+    before_action :authorize
+
+    private
+    def authorize
+        render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    end
 
 end
